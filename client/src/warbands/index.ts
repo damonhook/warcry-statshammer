4ac09328aafa068e7aa44d58511bd7a64e8b3234
Damon Hook <damon.hook@takealot.com>
2020-02-07 13:45:24 +0200

    Add all Season 1 Warcry Warbands

--- client/src/warbands/index.ts
+++ client/src/warbands/index.ts
@@ -1,7 +1,11 @@
+import CorvusCabal from './CorvusCabal';
+import CypherLords from './CypherLords';
 import IronGolems from './IronGolems';
+import SplinteredFang from './SplinteredFang';
+import TheUnmade from './TheUnmade';
 import UntamedBeasts from './UntamedBeasts';
 import { IWarband } from './warbands.types';
 
-const warbands: IWarband[] = [IronGolems, UntamedBeasts].sort((a, b) => a.name.localeCompare(b.name));
+const warbands: IWarband[] = [CorvusCabal, CypherLords, IronGolems, SplinteredFang, TheUnmade, UntamedBeasts];
 
-export default warbands;
+export default warbands.sort((a, b) => a.name.localeCompare(b.name));
