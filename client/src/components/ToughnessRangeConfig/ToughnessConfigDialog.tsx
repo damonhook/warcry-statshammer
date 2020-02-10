0485934ccd902cfd80a01d6b96a720776943b6e0
Damon Hook <damon.hook@takealot.com>
2020-02-10 23:22:01 +0200

    Styling changes

--- client/src/components/ToughnessRangeConfig/ToughnessConfigDialog.tsx
+++ client/src/components/ToughnessRangeConfig/ToughnessConfigDialog.tsx
@@ -1,4 +1,5 @@
 import {
+  AppBar,
   Button,
   Checkbox,
   Collapse,
@@ -8,12 +9,15 @@ import {
   DialogTitle,
   FormControlLabel,
   Grid,
+  IconButton,
   Paper,
   Slider,
+  Toolbar,
   Typography,
 } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { Check, Close, Refresh, Warning } from '@material-ui/icons';
+import clsx from 'clsx';
 import { useHashMatch } from 'hooks';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch } from 'react-redux';
@@ -22,15 +26,33 @@ import { config as configStore } from 'store/slices';
 import { TToughnessConfig } from 'types/config';
 import { HASHES } from 'utils/urls';
 
+import { useIsMobile } from '../../hooks/useIsMobile';
 import { getToughnessRange, getWarning, isAuto } from './utils';
 
 const useStyles = makeStyles((theme: Theme) => ({
+  title: {
+    display: 'flex',
+  },
+  closeIcon: {
+    marginRight: theme.spacing(1),
+  },
   sliderContainer: {
     paddingTop: theme.spacing(5),
     marginBottom: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {
+      paddingTop: theme.spacing(2),
+    },
+  },
+  autoLabel: {
+    [theme.breakpoints.down('sm')]: {
+      margin: theme.spacing(0, 0.5),
+    },
   },
   slider: {
     flex: 1,
+    [theme.breakpoints.down('sm')]: {
+      marginTop: theme.spacing(3),
+    },
   },
   warning: {
     background: theme.palette.background.nested,
@@ -39,6 +61,17 @@ const useStyles = makeStyles((theme: Theme) => ({
   warningTitle: {
     marginBottom: theme.spacing(0.5),
   },
+  minAuto: {
+    '& $thumb[data-index="0"]': {
+      color: theme.palette.grey[400],
+    },
+  },
+  maxAuto: {
+    '& $thumb[data-index="1"]': {
+      color: theme.palette.grey[400],
+    },
+  },
+  thumb: {},
 }));
 
 interface IToughnessConfigDialogProps {
@@ -51,6 +84,7 @@ const ToughnessConfigDialog = ({ toughnessConfig, minStr, maxStr }: IToughnessCo
   const classes = useStyles();
   const history = useHistory();
   const dispatch = useDispatch();
+  const mobile = useIsMobile();
 
   const [min, setMin] = useState(1);
   const [max, setMax] = useState(1);
@@ -115,26 +149,70 @@ const ToughnessConfigDialog = ({ toughnessConfig, minStr, maxStr }: IToughnessCo
   const value: [number, number] = [min, max];
 
   return (
-    <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
-      <DialogTitle>Customize Toughness Range</DialogTitle>
+    <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md" fullScreen={mobile}>
+      {mobile ? (
+        <AppBar position="relative">
+          <Toolbar>
+            <IconButton onClick={handleClose} className={classes.closeIcon}>
+              <Close />
+            </IconButton>
+            <Typography variant="h6">Customize Toughness Ranges</Typography>
+          </Toolbar>
+        </AppBar>
+      ) : (
+        <DialogTitle className={classes.title}>
+          <IconButton onClick={handleClose} size="small" className={classes.closeIcon}>
+            <Close />
+          </IconButton>
+          <span>Customize Toughness Ranges</span>
+        </DialogTitle>
+      )}
       <DialogContent>
-        <Typography>Explanation</Typography>
-        <Grid container alignItems="center" className={classes.sliderContainer} spacing={2}>
+        <Typography>
+          {`How the Auto range is deternined: Since Warcry only cares about Strength <, =, or > Toughness.
+          The Auto mode determines the range as follows:`}
+        </Typography>
+        <br />
+        <Typography variant="button">Range = (Lowest STR - 1) - (Highest STR + 1)</Typography>
+        <br />
+        <br />
+        <Typography>
+          <span>I recommend that you leave both the min and max as </span>
+          <i>Auto</i>
+        </Typography>
+        <br />
+        <Grid container alignItems="center" className={classes.sliderContainer} spacing={1}>
           <Grid item>
             <FormControlLabel
               label="Auto"
-              labelPlacement="start"
+              labelPlacement={mobile ? 'top' : 'start'}
               control={<Checkbox color="primary" checked={minAuto} onChange={handleMinAutoChanged} />}
+              className={classes.autoLabel}
             />
           </Grid>
           <Grid item className={classes.slider}>
-            <Slider min={1} max={10} value={value} valueLabelDisplay="on" onChange={handleChange} />
+            <Slider
+              min={1}
+              max={10}
+              value={value}
+              valueLabelDisplay="on"
+              onChange={handleChange}
+              classes={{
+                root: clsx({
+                  [classes.minAuto]: minAuto && min !== max,
+                  [classes.maxAuto]: maxAuto && min !== max,
+                }),
+                thumb: classes.thumb,
+              }}
+              disabled={minAuto && maxAuto}
+            />
           </Grid>
           <Grid item>
             <FormControlLabel
               label="Auto"
-              labelPlacement="end"
+              labelPlacement={mobile ? 'top' : 'end'}
               control={<Checkbox color="primary" checked={maxAuto} onChange={handleMaxAutoChanged} />}
+              className={classes.autoLabel}
             />
           </Grid>
         </Grid>
